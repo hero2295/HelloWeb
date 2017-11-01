c000378192025950ccc6a8f3fe31de108bbe7e14
hero2295 <geralex9595@gmail.com>
2017-11-02 01:09:54 +0500

    Added new method initData.

--- src/main/java/controller/HelloController.java
+++ src/main/java/controller/HelloController.java
@@ -1,10 +1,12 @@
 package controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HelloController {
@@ -12,6 +14,10 @@ public class HelloController {
     public String Calculate(@RequestParam int chislo1, @RequestParam int chislo2, ModelMap model) {
         int res = chislo1 * chislo2;
         model.addAttribute("message1", res);
+        return "hello";
+    }
+    @RequestMapping(value="/index")
+    public String initData(Model model){
         return "hello";
     }
 }
